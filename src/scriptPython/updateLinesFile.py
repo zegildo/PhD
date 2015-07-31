7b671fa287d137971535a45256a6eaf5a8cb8cf7
zegildo <zegildo@gmail.com>
2015-07-31 12:05:57 -0300

    noticiasPolaridade

--- src/scriptPython/updateLinesFile.py
+++ src/scriptPython/updateLinesFile.py
@@ -10,12 +10,13 @@ collection = db.informacoesGerais
 
 with open('noticiasP5.txt','r') as f:
 	while True:
-		group = list(islice(f,6))
+		group = list(islice(f,2))
 		if not group:
 			break
 		id = group[0].rstrip()
 		print 'Id: ', id
-		title = group[2].rstrip()
-		content = group[4].rstrip()
-		collection.find_one_and_update({'_id':ObjectId(id)},{'$set':{'title':title,'content':content}})
+		content = group[1].rstrip()
+		print 'Content: ', content
+		#content = group[4].rstrip()
+		#collection.find_one_and_update({'_id':ObjectId(id)},{'$set':{'polarity':polarity}})
 
