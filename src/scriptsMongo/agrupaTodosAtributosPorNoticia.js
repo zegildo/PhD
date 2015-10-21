93d32efba274000003e58d802ae22acda49864f6
zegildo <zegildo@gmail.com>
2015-10-21 17:05:13 -0300

    novas abordagens de modelos de classificacao

---
+++ src/scriptsMongo/agrupaTodosAtributosPorNoticia.js
@@ -0,0 +1,48 @@
+
+print("Data,DiadaSemana,Mes,Dia,Ano,Jornal,qtComentarios,qtTweets,qtFacebook,qtLinkedIn,qtGooglePlus,polaridade");
+
+db.informacoesGerais.find({}).forEach(
+
+	function (doc){
+		
+		var subFonte = doc.subFonte;
+		var polaridade = doc.polarity;
+
+		if(["ESTADAO","FOLHASP","G1"].indexOf(subFonte) > -1){
+			var result = "";
+			var date = new Date(doc.timestamp * 1000);        
+			var diaSemana = (date.getDay()+1);
+			var mes = (date.getMonth()+1)+"";
+			var mesData = mes
+			if(mes.length < 2){
+				mesData =  "0"+mes;
+			}
+			var dia = date.getDate()+"";
+			var diaData = dia
+			if(dia.length < 2){
+				diaData = "0"+dia;
+			}			
+			var ano = date.getFullYear();
+			var data = diaData+"/"+mesData+"/"+ano;
+			result += data+","+diaSemana+","+dia+","+mes+","+ano+","+subFonte+",";
+			var repercussoes = doc.repercussao.split(",");
+			var engajamento = {"c":0,"t":0,"f":0,"l":0,"g":0};
+			for (var rep in repercussoes){
+				siglaValor = repercussoes[rep].split(":");
+				sigla = siglaValor[0];
+				if(sigla != "total"){
+					valor = siglaValor[1];
+					engajamento[sigla] = valor;
+				}
+
+			}
+			for (var i in engajamento){
+				result+=engajamento[i]+",";
+			}
+			result+=polaridade;
+			print(result);
+
+		}
+	}
+
+);
